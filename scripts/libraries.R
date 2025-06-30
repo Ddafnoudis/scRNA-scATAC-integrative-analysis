d1a44cbc8e09cc0b139f3e967d33f37b97463383
Dimitris Dafnoudis <dimitrisdaf@hotmail.com>
2025-06-30 12:42:01 +0200

    scripts in

---
+++ scripts/libraries.R
@@ -0,0 +1,11 @@
+library(Seurat, help, pos = 2, lib.loc = NULL)
+# if (!require("BiocManager", quietly = TRUE))
+#     install.packages("BiocManager")
+
+# BiocManager::install("SingleR")
+
+library(SingleR, help, pos = 2, lib.loc = NULL)
+library(Signac, help, pos = 2, lib.loc = NULL)
+# install.packages("Signac")
+install.packages("hdf5r")
+
